a1c2248b8ef33aadc9b9116606753ff6dc7dd520
mundane <799699348@qq.com>
2023-12-01 20:35:08 +0800

    第10章

---
+++ mock/data/getQuestionList.js
@@ -0,0 +1,26 @@
+/**
+ * @description 生成问卷列表
+ */
+
+const Mock = require("mockjs");
+const Random = Mock.Random;
+
+function getQuestionList(opt = {}) {
+  const { len = 10, isDeleted = false, isStar } = opt;
+  console.log(123, isDeleted, isStar);
+  const list = [];
+  for (let i = 0; i < len; i++) {
+    list.push({
+      _id: Random.id(),
+      title: Random.ctitle(),
+      isPublished: Random.boolean(),
+      isStar: isStar ?? Random.boolean(),
+      answerCount: Random.natural(50, 100),
+      createdAt: Random.datetime(),
+      isDeleted: isDeleted ?? Random.boolean(),
+    });
+  }
+  return list;
+}
+
+module.exports = getQuestionList;
---
+++ mock/question.js
@@ -0,0 +1,59 @@
+const Mock = require("mockjs");
+const getQuestionList = require("./data/getQuestionList");
+
+const Random = Mock.Random;
+
+module.exports = [
+  {
+    url: "/api/question/:id",
+    method: "get",
+    response() {
+      return {
+        errno: 0,
+        data: {
+          id: Random.id(),
+          title: Random.ctitle(),
+        },
+        // errno: 1002,
+        // msg: "题目不存在",
+      };
+    },
+  },
+  {
+    url: "/api/question",
+    method: "post",
+    response() {
+      return {
+        errno: 0,
+        data: {
+          id: Random.id(),
+        },
+      };
+    },
+  },
+  {
+    // 获取（查询）问卷列表
+    url: "/api/question",
+    method: "get",
+    response(ctx) {
+      const { query } = ctx;
+      const isDeleted = isUndefinedOrNull(query.isDeleted)
+        ? query.isDeleted
+        : query.isDeleted === "true";
+      const isStar = isUndefinedOrNull(query.isStar)
+        ? query.isStar
+        : query.isStar === "true";
+
+      function isUndefinedOrNull(value) {
+        return value === undefined || value === null;
+      }
+      return {
+        errno: 0,
+        data: {
+          list: getQuestionList({ isDeleted, isStar }),
+          total: 100,
+        },
+      };
+    },
+  },
+];
