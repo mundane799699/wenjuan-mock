b5c540761083069a74547c7510cea17a3953d1d9
mundane <799699348@qq.com>
2023-12-09 21:30:30 +0800

    第14章完成

--- mock/question.js
+++ mock/question.js
@@ -17,8 +17,21 @@ module.exports = [
           componentList: [
             {
               fe_id: Random.id(),
+              type: "questionInfo",
+              title: "问卷信息",
+              isHidden: false,
+              isLocked: false,
+              props: {
+                title: "问卷标题",
+                desc: "问卷描述",
+              },
+            },
+            {
+              fe_id: Random.id(),
               type: "questionTitle",
               title: "标题",
+              isHidden: false,
+              isLocked: false,
               props: {
                 text: "个人信息调研",
                 level: 1,
@@ -29,6 +42,8 @@ module.exports = [
               fe_id: Random.id(),
               type: "questionInput",
               title: "输入框1",
+              isHidden: false,
+              isLocked: false,
               props: {
                 title: "你的姓名",
                 placeholder: "请输入姓名...",
@@ -38,9 +53,87 @@ module.exports = [
               fe_id: Random.id(),
               type: "questionInput",
               title: "输入框2",
+              isHidden: false,
+              isLocked: false,
               props: {
                 title: "你的电话",
                 placeholder: "请输入电话...",
+              },
+            },
+            {
+              fe_id: Random.id(),
+              type: "questionTextarea",
+              title: "多行输入",
+              isHidden: false,
+              isLocked: false,
+              props: {
+                title: "你的爱好",
+                placeholder: "请输入...",
+              },
+            },
+            {
+              fe_id: Random.id(),
+              type: "questionParagraph",
+              title: "段落",
+              isHidden: false,
+              isLocked: false,
+              props: {
+                title: "一行段落",
+                isCenter: false,
+              },
+            },
+            {
+              fe_id: Random.id(),
+              type: "questionRadio",
+              title: "单选",
+              isHidden: false,
+              isLocked: false,
+              props: {
+                title: "单选标题",
+                isVertical: false,
+                options: [
+                  {
+                    value: "item1",
+                    text: "选项1",
+                  },
+                  {
+                    value: "item2",
+                    text: "选项2",
+                  },
+                  {
+                    value: "item3",
+                    text: "选项3",
+                  },
+                ],
+                value: "",
+              },
+            },
+            {
+              fe_id: Random.id(),
+              type: "questionCheckbox",
+              title: "多选",
+              isHidden: false,
+              isLocked: false,
+              props: {
+                title: "多选标题",
+                isVertical: false,
+                list: [
+                  {
+                    value: "item1",
+                    text: "选项1",
+                    checked: false,
+                  },
+                  {
+                    value: "item2",
+                    text: "选项2",
+                    checked: false,
+                  },
+                  {
+                    value: "item3",
+                    text: "选项3",
+                    checked: false,
+                  },
+                ],
               },
             },
           ],
