dbffb33faa13718b3a65c6182cb772b883ea9cd1
mundane <799699348@qq.com>
2023-12-15 15:56:04 +0800

    第16章完成

--- mock/question.js
+++ mock/question.js
@@ -1,6 +1,6 @@
 const Mock = require("mockjs");
 const getQuestionList = require("./data/getQuestionList");
-
+const getComponentList = require("./data/getComponentList");
 const Random = Mock.Random;
 
 module.exports = [
@@ -17,129 +17,9 @@ module.exports = [
           desc: Random.cparagraph(),
           js: Random.cparagraph(),
           css: Random.cparagraph(),
-          componentList: [
-            {
-              fe_id: Random.id(),
-              type: "questionInfo",
-              title: "问卷信息",
-              isHidden: false,
-              isLocked: false,
-              props: {
-                title: "问卷标题",
-                desc: "问卷描述",
-              },
-            },
-            {
-              fe_id: Random.id(),
-              type: "questionTitle",
-              title: "标题",
-              isHidden: false,
-              isLocked: false,
-              props: {
-                text: "个人信息调研",
-                level: 1,
-                isCenter: false,
-              },
-            },
-            {
-              fe_id: Random.id(),
-              type: "questionInput",
-              title: "输入框1",
-              isHidden: false,
-              isLocked: false,
-              props: {
-                title: "你的姓名",
-                placeholder: "请输入姓名...",
-              },
-            },
-            {
-              fe_id: Random.id(),
-              type: "questionInput",
-              title: "输入框2",
-              isHidden: false,
-              isLocked: false,
-              props: {
-                title: "你的电话",
-                placeholder: "请输入电话...",
-              },
-            },
-            {
-              fe_id: Random.id(),
-              type: "questionTextarea",
-              title: "多行输入",
-              isHidden: false,
-              isLocked: false,
-              props: {
-                title: "你的爱好",
-                placeholder: "请输入...",
-              },
-            },
-            {
-              fe_id: Random.id(),
-              type: "questionParagraph",
-              title: "段落",
-              isHidden: false,
-              isLocked: false,
-              props: {
-                title: "一行段落",
-                isCenter: false,
-              },
-            },
-            {
-              fe_id: Random.id(),
-              type: "questionRadio",
-              title: "单选",
-              isHidden: false,
-              isLocked: false,
-              props: {
-                title: "单选标题",
-                isVertical: false,
-                options: [
-                  {
-                    value: "item1",
-                    text: "选项1",
-                  },
-                  {
-                    value: "item2",
-                    text: "选项2",
-                  },
-                  {
-                    value: "item3",
-                    text: "选项3",
-                  },
-                ],
-                value: "",
-              },
-            },
-            {
-              fe_id: Random.id(),
-              type: "questionCheckbox",
-              title: "多选",
-              isHidden: false,
-              isLocked: false,
-              props: {
-                title: "多选标题",
-                isVertical: false,
-                list: [
-                  {
-                    value: "item1",
-                    text: "选项1",
-                    checked: false,
-                  },
-                  {
-                    value: "item2",
-                    text: "选项2",
-                    checked: false,
-                  },
-                  {
-                    value: "item3",
-                    text: "选项3",
-                    checked: false,
-                  },
-                ],
-              },
-            },
-          ],
+          isPublished: true,
+          // isPublished: false,
+          componentList: getComponentList(),
         },
         // errno: 1002,
         // msg: "题目不存在",
